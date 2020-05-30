c0d59d86157b87ef62d482016923cd68d470bafe
Farran <57780709+farran94@users.noreply.github.com>
2020-05-31 02:02:58 +1200

    External JS for materialize initializtion

---
+++ js/materialize.js
@@ -0,0 +1,12 @@
+(function () {
+    window.onload = function(){
+        var frameName = new ds07o6pcmkorn({
+            openElementId: "#shielded-logo"
+        });
+        frameName.init();
+
+        $('.modal').modal();
+
+        $(".button-collapse").sideNav();
+    }
+})();
